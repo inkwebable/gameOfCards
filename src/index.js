974dfb644af2add54b02457d3000edfe0009ca26
Thien Nguyen <newtj2@gmail.com>
2020-05-20 21:04:10 +0100

    Exporting GameOfCards & package.json update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import styles from './assets/styles/style.scss';
-import { Deck } from './Deck';
 import { Game } from './Game';
 import * as Actions from './helpers/utils';
 
-// const GameOfCards = {
-//   Game,
-//   Deck,
-//   Actions
-// }
 
-export { Game, Deck, Actions };
+export { Actions };
+export * from './helpers/utils';
+export { Game as GameOfCards } from './Game';
