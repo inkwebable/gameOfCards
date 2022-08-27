3eb00f57e7f3cfd68fa78cd7306d3d00a8c80c2a
thiennguyen1 <newtj2@gmail.com>
2022-08-27 20:38:08 +0100

    Fixed wrong path syntax

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@ const path = require('path');
 const fs = require('fs-extra')
 
 let sourceDir = path.resolve(__dirname, 'dist/images/game-of-cards');
-let destinationDir = `${process.env.INIT_CWD}\\images\\game-of-cards`;
-const destinationAssetsDir = `${process.env.INIT_CWD}\\assets`;
-const destinationPublicDir = `${process.env.INIT_CWD}\\public`;
+let destinationDir = `${process.env.INIT_CWD}/images/game-of-cards`;
+const destinationAssetsDir = `${process.env.INIT_CWD}/assets`;
+const destinationPublicDir = `${process.env.INIT_CWD}/public`;
 
 console.log('destinationDir', destinationDir);
 console.log('sourceDir', sourceDir);
 
 if (fs.existsSync(destinationPublicDir)) {
-  destinationDir = `${process.env.INIT_CWD}\\public\\images\\game-of-cards`;
+  destinationDir = `${process.env.INIT_CWD}/public/images/game-of-cards`;
 } else if (fs.existsSync(destinationAssetsDir)) {
-  destinationDir = `${process.env.INIT_CWD}\\assets\\images\\game-of-cards`;
+  destinationDir = `${process.env.INIT_CWD}/assets/images/game-of-cards`;
 } else {
   if (!fs.existsSync(destinationDir)){
     fs.mkdirSync(destinationDir, { recursive: true });
